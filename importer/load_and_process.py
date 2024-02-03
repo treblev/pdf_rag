d35e0dc9454753da884314709cd89d6600c4c868
vvvijaya <v.vijayvignesh@gmail.com>
2024-02-02 19:57:54 -0700

    upload embeddings

--- importer/load_and_process.py
+++ importer/load_and_process.py
@@ -12,7 +12,7 @@ load_dotenv()
 
 loader = DirectoryLoader(
     #os.path.abspath("../source_docs"),
-    "C:\CodePlayground\Pdf_Rag\source_docs",
+    "~/Coding/Github/Pdf_Rag/source_docs",
     glob="**/*.pdf",
     show_progress=True,
     loader_cls=UnstructuredPDFLoader,
@@ -34,6 +34,6 @@ chunks = text_splitter.split_documents(docs)
 PGVector.from_documents(chunks
                         , embedding=embeddings
                         , collection_name= "pdf_rag"
-                        , connection_string = "postgresql://postgres.svvssjtshgpsrnudvsou:amr_vvvijaya@aws-0-us-west-1.pooler.supabase.com:5432/postgres" #"postgresql+psycopg://vvvijaya@127.0.0.1:5432/pdf_rag_vectors?password=vvvijaya"
+                        , connection_string = "postgresql://postgres.svvssjtshgpsrnudvsou:[]@aws-0-us-west-1.pooler.supabase.com:5432/postgres" #"postgresql+psycopg://vvvijaya@127.0.0.1:5432/pdf_rag_vectors?password=vvvijaya"
                         , pre_delete_collection= True
                         )
